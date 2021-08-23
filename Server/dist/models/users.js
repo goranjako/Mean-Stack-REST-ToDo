5435015f2c9d1e0cafee9a109011e2affe6d7979
Goran Jakovljević <goranjako@gmail.com>
2021-08-23 20:35:22 +0200

    trim

--- Server/dist/models/users.js
+++ Server/dist/models/users.js
@@ -9,19 +9,22 @@ var bcrypt = require('bcrypt-nodejs');
 var UserSchema = new Schema({
   fullName: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
     // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
     match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/, 'Please enter a valid email']
   },
   password: {
     type: String,
     required: true,
-    minlength: 4
+    minlength: 4,
+    trim: true
   }
 });
 UserSchema.pre('save', function (next) {
